c8667b98324fb50995e6c4c54eec417a3124e465
EashionY <silver_kira@live.com>
2017-07-21 15:30:27 +0800

    添加未找到对应用户异常

---
+++ src/main/java/com/rrenpin/service/NoUserFindException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.service;
+
+public class NoUserFindException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2320375957352872962L;
+
+	public NoUserFindException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoUserFindException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoUserFindException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoUserFindException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoUserFindException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
