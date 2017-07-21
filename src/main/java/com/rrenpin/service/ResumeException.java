206d51b01d63f747b160ee21c7c97d07266887fc
EashionY <silver_kira@live.com>
2017-07-21 18:19:22 +0800

    简历异常类

---
+++ src/main/java/com/rrenpin/service/ResumeException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.service;
+
+public class ResumeException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1885806597930929615L;
+
+	public ResumeException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResumeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResumeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResumeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResumeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
