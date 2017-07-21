ad7ecd6fb2dd3c1ca9a23738798c1d44e783f87b
EashionY <silver_kira@live.com>
2017-07-21 15:30:52 +0800

    添加密码相关异常

---
+++ src/main/java/com/rrenpin/service/PasswordException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.service;
+
+public class PasswordException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1023101102821996238L;
+
+	public PasswordException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
