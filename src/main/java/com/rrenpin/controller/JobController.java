a4681026c75bce0b96158dd92cd21cee53e54209
EashionY <silver_kira@live.com>
2017-07-21 18:18:39 +0800

    继承了ExceptionController

--- src/main/java/com/rrenpin/controller/JobController.java
+++ src/main/java/com/rrenpin/controller/JobController.java
@@ -15,7 +15,7 @@ import com.rrenpin.util.JsonResult;
 
 @Controller
 @RequestMapping("/job")
-public class JobController {
+public class JobController extends ExceptionController{
 
 	@Resource
 	private JobService jobService;
