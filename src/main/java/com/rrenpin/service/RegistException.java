2914a34fd7a04e7c96258c71368600d3dd0e934b
EashionY <silver_kira@live.com>
2017-07-25 16:40:34 +0800

    注册异常类

---
+++ src/main/java/com/rrenpin/service/RegistException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.service;
+
+public class RegistException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 78599569691062193L;
+
+	public RegistException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public RegistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RegistException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RegistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RegistException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
