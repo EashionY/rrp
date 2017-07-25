593f28286d5fdd6dac23e1408ed1fa92fbe7230e
EashionY <silver_kira@live.com>
2017-07-25 16:40:49 +0800

    发送验证码异常类

---
+++ src/main/java/com/rrenpin/service/SendCodeException.java
@@ -0,0 +1,32 @@
+package com.rrenpin.service;
+
+public class SendCodeException extends RuntimeException {
+
+	private static final long serialVersionUID = 461596050350104246L;
+
+	public SendCodeException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SendCodeException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SendCodeException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SendCodeException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SendCodeException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
