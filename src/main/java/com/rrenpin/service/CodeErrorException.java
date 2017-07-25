83dd010ed324c97659f9f6418ab8da4406fc3161
EashionY <silver_kira@live.com>
2017-07-25 16:40:01 +0800

    验证码异常类

---
+++ src/main/java/com/rrenpin/service/CodeErrorException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.service;
+
+public class CodeErrorException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -745514386294788419L;
+
+	public CodeErrorException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CodeErrorException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CodeErrorException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CodeErrorException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CodeErrorException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
