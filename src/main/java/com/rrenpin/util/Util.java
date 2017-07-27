21a59971fb00863647d3d832b828e0bcc13a1ae7
EashionY <silver_kira@live.com>
2017-07-27 10:47:26 +0800

    配置

--- src/main/java/com/rrenpin/util/Util.java
+++ src/main/java/com/rrenpin/util/Util.java
@@ -1,12 +1,18 @@
 package com.rrenpin.util;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 
 public class Util {
 
-	//对字符串进行MD5加密
+	/**
+	 * 对字符串进行MD5加密
+	 * @param src
+	 * @return
+	 */
 	public static String md5(String src){
 		try {
 			MessageDigest md= MessageDigest.getInstance("MD5");
@@ -18,5 +24,48 @@ public class Util {
 			throw new Md5Exception("加密失败", e);
 		}
 	}
+	
+	/** 
+     * 将源字符串使用MD5加密为字节数组 
+     * @param source 
+     * @return 
+     */  
+    public static byte[] encode2bytes(String source) {  
+        byte[] result = null;  
+        try {  
+            MessageDigest md = MessageDigest.getInstance("MD5");  
+            md.reset();  
+            md.update(source.getBytes("UTF-8"));  
+            result = md.digest();  
+        } catch (NoSuchAlgorithmException e) {  
+            e.printStackTrace();  
+        } catch (UnsupportedEncodingException e) {  
+            e.printStackTrace();  
+        }  
+          
+        return result;  
+    } 
+    
+	/** 
+     * 将源字符串使用MD5加密为32位16进制数 
+     * @param source 
+     * @return 
+     */  
+    public static String encode2hex(String source) {  
+        byte[] data = encode2bytes(source);  
+  
+        StringBuffer hexString = new StringBuffer();  
+        for (int i = 0; i < data.length; i++) {  
+            String hex = Integer.toHexString(0xff & data[i]);  
+              
+            if (hex.length() == 1) {  
+                hexString.append('0');  
+            }  
+              
+            hexString.append(hex);  
+        }  
+          
+        return hexString.toString();  
+    }  
 	
 }
