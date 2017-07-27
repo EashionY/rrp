4d5d64497c2e1130e8dfc663818a79c00c882f18
EashionY <silver_kira@live.com>
2017-07-27 10:50:00 +0800

    邮箱异常类

---
+++ src/main/java/com/rrenpin/service/EmailException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.service;
+
+public class EmailException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7772765166701746424L;
+
+	public EmailException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmailException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
