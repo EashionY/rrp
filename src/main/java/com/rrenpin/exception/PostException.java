1a866a775e4e85629c2ea4c8a3e15949cb1d4018
EashionY <silver_kira@live.com>
2017-08-01 18:51:27 +0800

    职位发布管理异常类

---
+++ src/main/java/com/rrenpin/exception/PostException.java
@@ -0,0 +1,35 @@
+package com.rrenpin.exception;
+
+public class PostException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3254647218290860796L;
+
+	public PostException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PostException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PostException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PostException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PostException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
