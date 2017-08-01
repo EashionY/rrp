7dcaf85a16179c98a3e12070f5f04de03990f8a1
EashionY <silver_kira@live.com>
2017-08-01 18:51:05 +0800

    增加方法：List<Post> listPostJob()

--- src/main/java/com/rrenpin/dao/PostMapper.java
+++ src/main/java/com/rrenpin/dao/PostMapper.java
@@ -1,5 +1,9 @@
 package com.rrenpin.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.rrenpin.entity.Post;
 
 public interface PostMapper {
@@ -14,4 +18,17 @@ public interface PostMapper {
     int updateByPrimaryKeySelective(Post record);
 
     int updateByPrimaryKey(Post record);
+    /**
+     * 获取表中的最大id(即为最后插入数据的id)
+     * @return
+     */
+    int getMaxId();
+    
+    /**
+     * 公司发布的职位集合
+     * @param companyId
+     * @param status 状态码
+     * @return
+     */
+    List<Post> listPostJob(@Param("companyId")int companyId,@Param("status")String status);
 }
