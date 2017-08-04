27fda85f80f5a143af66ab8fbb0938de837ec76b
Administrator <Administrator@192.168.0.104>
2017-08-04 17:16:17 +0800

    完成求职页面多条件查询分页

--- src/main/webapp/js/job/apply_job.js
+++ src/main/webapp/js/job/apply_job.js
@@ -1,3 +1,65 @@
+function addDom(mydata){
+	$.get(ip+"/rrp/post/searchPostCompany.do",mydata,function(data){
+		if(data.state==0){
+			console.log(data.data);
+			var result=data.data;
+			$(".zhiwei_Box").html("");
+			if(result.length==0){
+				layer.msg("对不起，没有符合所选条件的结果！");
+			}else{
+				var str="";
+				var myDate = new Date();
+				var nowDate=new Date(myDate.toLocaleDateString()).format("yyyy/MM/dd");//当前时间
+				$.each(result,function(k,v){
+					   var postDate=new Date(result[k].post_time).format("yyyy/MM/dd");
+					   var showDate=''
+					   if(nowDate==postDate){//当天发布
+						   showDate=new Date(result[k].post_time).format("hh:mm")
+					   }else{
+						   showDate=new Date(result[k].post_time).format("yy年MM月dd日")
+					   }
+					str+='<div class="zhiwei_box"><input class="myinput" value="'+result[k].post_id+'"/><div class="zhiwei_mainbox">'+
+	                '<div class="top"><div><ul><li>'+result[k].post_name+'</li><li>['+result[k].region+']</li><li>'+showDate+'  发布</li></ul>'+
+	                '<div class="salary_text">'+result[k].salary+'</div></div>'+
+	                '<div class="comName">'+result[k].name+'</div><input class="myinput" value="'+result[k].company_id+'"/></div>'+
+	                '<div class="bottom"><ul><li>经验'+result[k].work_exp+'</li><li>'+result[k].degree+'</li></ul><div>'+result[k].industry+'/'+result[k].financing+'</div></div></div> </div>';
+				});
+				$(".zhiwei_Box").html(str);
+				
+				$(".zhiwei_box").click(function(){
+	            	window.location.href="../job/job_detail.html?postId="+$(this).children("input").val();
+	            })
+	            $(".comName").click(function(event){
+	            	window.location.href="../company/company.html?companyId="+$(this).next().val();
+	            	event.stopPropagation();//阻止事件冒泡
+	            });
+			}
+		}else{
+			layer.msg(data.message);
+		}
+	},'json')
+}
+function getTotal(mydata){//利用同步获取总条数
+	var totalNum=0;
+	$.ajax({
+		type:"get",
+		url:ip+"/rrp/post/searchPostCompany.do",
+		data:mydata,
+		async: false,
+		success:function(data) {
+			if(data.state==0){
+				var result=data.data;
+				if(result.length==0){
+					layer.msg("对不起，没有符合所选条件的结果！");
+				}else{
+					totalNum=result[0].totalnum;
+				}
+			}
+		}
+	})
+	return totalNum;
+}
+
 function find(obj){
 	var name='';
 	$.each(obj,function(k,v){
@@ -26,25 +88,9 @@ function xunhuan(tjArr,obj,num){
 			}else{
 				tjArr.splice(num,1,tj);//替换元素
 			}
-			console.log(tjArr);
-			$.get(ip+"/rrp/post/searchPostCompany.do",{keyword:tjArr[0],region:tjArr[1],workExp:tjArr[2],degree:tjArr[3],scale:tjArr[4],salary:tjArr[5],industry:tjArr[6],financing:tjArr[7],page:1,pageSize:8},function(data){
-				if(data.state==0){
-					console.log(data.data);
-					var result=data.data;
-					$(".zhiwei_Box").html("");
-					var str="";
-					$.each(result,function(){
-						str+='<div class="zhiwei_box"><div class="zhiwei_mainbox">'+
-                        '<div class="top"><div><ul><li>ios开发工程师</li><li>[成都]</li><li>14:28发布</li></ul>'+
-                        '<div class="salary_text">10K-20K</div></div>'+
-                        '<div class="comName">凯利特网络</div></div>'+
-                        '<div class="bottom"><ul><li>经验1-3年</li><li>本科</li></ul><div>电子商务/初创型(未融资)</div></div></div> </div>';
-					});
-					$(".zhiwei_Box").html(str);
-				}else{
-					layer.msg(data.message);
-				}
-			},'json')
+			//console.log(tjArr);
+			var mydata={keyword:tjArr[0],region:tjArr[1],workExp:tjArr[2],degree:tjArr[3],scale:tjArr[4],salary:tjArr[5],industry:tjArr[6],financing:tjArr[7],page:1,pageSize:3}
+			add_page(mydata);
 		})
 	})
 }
@@ -57,19 +103,65 @@ function shijian(obj1,obj2,obj3,obj4,obj5,obj6){
 	xunhuan(tjArr,obj5,6);
 	xunhuan(tjArr,obj6,7);
 }
-
+function add_page(mydata){
+	addDom(mydata);//添加节点
+	var totalnum=getTotal(mydata);//总条数
+	console.log(totalnum)
+	var totalPage=Math.ceil(totalnum/mydata.pageSize);//总页数(向上取整);
+    //分页
+	layui.use(['laypage', 'layer'], function(){
+	     var laypage = layui.laypage,layer= layui.layer;
+	     laypage({
+	        cont: 'appJob_pages',
+	        pages: totalPage,//总页数
+	        first: 1,//首页
+	        last: totalPage,//末页
+	        prev: '<em><</em>',
+	        next: '<em>></em>',
+	        groups: 3,//连续显示分页数
+	        skin: '#3eb39d',
+	        jump: function(obj,first){
+	        	if(!first){
+	        		mydata.page=obj.curr;
+	        		console.log(mydata.page);
+	        		console.log(mydata)
+	        		addDom(mydata);
+	        	}
+	        }
+	    });
+	});
+}
 $(function() { 
 	myonload2("../my/pers_infor.html","../../Enterprise_edition/talent_recom.html","../index.html");
 	shijian($("#app_jyul li"),$("#app_xlul li"),$("#app_gmul li"),$("#app_xzul li"),$("#app_hyul li"),$("#app_rzul li"));
-	
+	var mydata={keyword:null,region:null,workExp:null,degree:null,scale:null,salary:null,industry:null,financing:null,page:1,pageSize:2}
+	add_page(mydata);
 });
-$(".appJob_serch_tubox").click(function(){
+
+
+$(".appJob_serch_tubox").click(function(){//搜索按钮
 	var keyword=$(".appJob_serInp").val();
 	var region=$("#ucity").val();
 	if(region==""&&keyword==""){
 		layer.msg("搜索条件为空！")
 	}else{
-		console.log(region)
-		console.log(keyword)
+		//console.log(region)
+		//console.log(keyword);
+		var mydata={keyword:keyword,region:region,workExp:null,degree:null,scale:null,salary:null,industry:null,financing:null,page:1,pageSize:3}
+		add_page(mydata);
 	}
 })
+
+
+$(".cond_ul>li").on("click",function(){//样式转换
+        var liList=$(this).parent().children();
+        var nowId=$(this).index();
+        $.each(liList,function(key,val){
+            if(key==nowId){
+                $(val).addClass("cond_selected")
+            }else{
+                $(val).removeClass("cond_selected")
+            }
+        });
+ })
+   
